5677596cf3d5cad821357839ed79f8818220d7cd
Natasza Nowakowska <natasza.nowakowska@postnord.com>
2024-11-10 16:48:57 +0100

    update to new fields in schema

--- server/models/sample.js
+++ server/models/sample.js
@@ -8,12 +8,20 @@ const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
   _id: String,
-  name: {
+  firstName: {
     type: String,
-    required: [true, 'Name is required.'],
+    required: [true, 'First name is required.'],
     trim: true,
-    minlength: [1, 'Name must have at least one character.'],
-    maxlength: [20, 'Name must have at most 20 characters.'],
+    minlength: [1, 'First name must have at least one character.'],
+    maxlength: [20, 'First name must have at most 20 characters.'],
+    default: '',
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required.'],
+    trim: true,
+    minlength: [1, 'Last name must have at least one character.'],
+    maxlength: [20, 'Last name must have at most 20 characters.'],
     default: '',
   },
 })
